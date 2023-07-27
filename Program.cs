c2458a2e890c9da75bc917d31277066fee89a840
jutakozi <jutakozi@gmail.com>
2023-07-27 17:53:43 +0200

    visible hits on the board

--- Program.cs
+++ Program.cs
@@ -4,80 +4,62 @@ namespace BattleshipsGame
 {
     class Program
     {
-        static char[,] board = new char[10, 10]; // Tworzenie dwuwymiarowej tablicy do przechowywania stanu planszy
-        static int shipsRemaining = 5; // Liczba pozostałych statków do zatopienia
-        static bool debugMode = false; // Flaga określająca tryb debugowania (widoczność statków)
+        static char[,] boardPlayer1 = new char[10, 10]; // Plansza gracza 1
+        static char[,] boardPlayer2 = new char[10, 10]; // Plansza gracza 2
+        static int shipsRemainingPlayer1 = 5; // Liczba pozostałych statków gracza 1
+        static int shipsRemainingPlayer2 = 5; // Liczba pozostałych statków gracza 2
+        static bool debugMode = false; // Tryb debugowania (widoczność statków)
+        static bool player1Turn = true; // Zmienna wskazująca, czy jest tura gracza 1
 
         static void Main(string[] args)
         {
-            InitializeBoard(); // Inicjalizacja planszy
-            PlaceShips(); // Umieszczanie statków na planszy
+            InitializeBoard(boardPlayer1); // Inicjalizacja planszy dla gracza 1
+            InitializeBoard(boardPlayer2); // Inicjalizacja planszy dla gracza 2
+            PlaceShips(boardPlayer1); // Umieszczenie statków na planszy gracza 1
+            PlaceShips(boardPlayer2); // Umieszczenie statków na planszy gracza 2
 
-            while (shipsRemaining > 0) // Pętla gry, działająca dopóki są jeszcze statki do zatopienia
+            // Pętla główna gry, działająca dopóki jednemu z graczy nie zostaną zatopione wszystkie statki
+            while (shipsRemainingPlayer1 > 0 && shipsRemainingPlayer2 > 0)
             {
-                DisplayBoard(); // Wyświetlanie planszy
-                Console.WriteLine("Enter row and column (e.g., A5): ");
-                string input = Console.ReadLine(); // Odczytywanie wiersza i kolumny wprowadzonych przez gracza
-
-                if (input.ToLower() == "debug")
+                if (player1Turn)
                 {
-                    debugMode = !debugMode; // Przełączanie trybu debugowania (widoczności statków)
-                    Console.WriteLine("Debug mode: " + (debugMode ? "Enabled" : "Disabled"));
-                    continue; // Kontynuowanie pętli
-                }
-
-                if (input.Length != 2)
-                {
-                    Console.WriteLine("Invalid input. Try again."); // Walidacja wprowadzonych danych
-                    continue; // Kontynuowanie pętli
-                }
-
-                int row = input[0] - 'A'; // Konwersja wiersza na liczbę  Litera 'A' ma wartość liczbową 65 w kodzie ASCII, więc odejmując 65 od wartości znaku wiersza, otrzymujemy wartość liczbową odpowiadającą indeksowi wiersza na planszy. Dzięki temu A odpowiada indeksowi 0, B odpowiada indeksowi 1, C odpowiada indeksowi 2 itd.
-                int col = input[1] - '0'; // Konwersja kolumny na liczbę  Cyfry w kodzie ASCII mają kolejne wartości liczbowe od 48 (0) do 57 (9), więc odejmując 48 od wartości znaku kolumny, otrzymujemy wartość liczbową odpowiadającą indeksowi kolumny na planszy.
-                //Dzięki tym konwersjom możemy uzyskać wartości liczbowe
-                //reprezentujące wiersz i kolumnę wprowadzone przez użytkownika,
-                //które są wykorzystywane do indeksowania tablicy planszy.
-
-
-
-                if (row < 0 || row >= 10 || col < 0 || col >= 10)
-                {
-                    Console.WriteLine("Invalid input. Try again."); // Walidacja wprowadzonych danych
-                    continue; // Kontynuowanie pętli
-                }
-
-                if (board[row, col] == 'X')
-                {
-                    Console.WriteLine("You hit a ship!"); // Gracz trafił statek
-                    board[row, col] = '!'; // Oznaczenie trafionego pola na planszy
-                    shipsRemaining--; // Zmniejszenie liczby pozostałych statków
-                }
-                else if (board[row, col] == '!')
-                {
-                    Console.WriteLine("You already hit this spot. Try again."); // Gracz już trafił to pole wcześniej
+                    Console.WriteLine("Player 1's Turn");
+                    PlayerTurn(boardPlayer2); // Tura gracza 1
                 }
                 else
                 {
-                    Console.WriteLine("You missed."); // Gracz chybił
-                    board[row, col] = '-'; // Oznaczenie chybionego pola na planszy
+                    Console.WriteLine("Player 2's Turn");
+                    PlayerTurn(boardPlayer1); // Tura gracza 2
                 }
+
+                player1Turn = !player1Turn; // Zmiana tury na przeciwnika po każdej rundzie
             }
 
-            Console.WriteLine("Congratulations! You sank all the ships!"); // Komunikat o zakończeniu gry po zatopieniu wszystkich statków
+            // Wyświetlenie wyniku gry
+            if (shipsRemainingPlayer1 == 0)
+            {
+                Console.WriteLine("Player 2 wins! All ships of Player 1 have been sunk.");
+            }
+            else
+            {
+                Console.WriteLine("Player 1 wins! All ships of Player 2 have been sunk.");
+            }
         }
 
-        static void InitializeBoard()
+        // Inicjalizacja planszy ustawiająca wszystkie pola na "-"
+        static void InitializeBoard(char[,] board)
         {
             for (int row = 0; row < 10; row++)
             {
                 for (int col = 0; col < 10; col++)
                 {
-                    board[row, col] = '-'; // Inicjalizacja planszy ustawiająca wszystkie pola na "-"
+                    board[row, col] = '-';
                 }
             }
         }
 
-        static void PlaceShips()
+        // Umieszczenie 5 statków na planszy
+        static void PlaceShips(char[,] board)
         {
             Random random = new Random();
 
@@ -88,7 +70,7 @@ namespace BattleshipsGame
 
                 if (board[row, col] == '-')
                 {
-                    board[row, col] = 'X'; // Umieszczanie statku na planszy
+                    board[row, col] = 'X'; // Umieszczenie statku na planszy
                 }
                 else
                 {
@@ -97,9 +79,63 @@ namespace BattleshipsGame
             }
         }
 
-        static void DisplayBoard()
+        // Funkcja obsługująca turę gracza
+        static void PlayerTurn(char[,] targetBoard)
         {
-            Console.WriteLine("   0 1 2 3 4 5 6 7 8 9"); // Wyświetlanie nagłówka kolumn
+            DisplayBoard(targetBoard); // Wyświetlenie planszy
+
+            Console.WriteLine("Enter row and column (e.g., A5): ");
+            string input = Console.ReadLine(); // Odczytanie wiersza i kolumny wprowadzonej przez gracza
+
+            // Sprawdzenie czy gracz chce włączyć tryb debugowania
+            if (input.ToLower() == "debug")
+            {
+                debugMode = !debugMode; // Przełączanie trybu debugowania (widoczności statków)
+                Console.WriteLine("Debug mode: " + (debugMode ? "Enabled" : "Disabled"));
+                PlayerTurn(targetBoard); // Rekurencyjne wywołanie tury dla tego samego gracza
+                return; // Zakończenie funkcji w przypadku trybu debugowania
+            }
+
+            // Walidacja wprowadzonych danych
+            if (input.Length != 2)
+            {
+                Console.WriteLine("Invalid input. Try again.");
+                PlayerTurn(targetBoard); // Rekurencyjne wywołanie tury dla tego samego gracza
+                return;
+            }
+
+            int row = input[0] - 'A'; // Konwersja wiersza na liczbę
+            int col = input[1] - '0'; // Konwersja kolumny na liczbę
+
+            // Sprawdzenie czy wprowadzone dane są w zakresie planszy
+            if (row < 0 || row >= 10 || col < 0 || col >= 10)
+            {
+                Console.WriteLine("Invalid input. Try again.");
+                PlayerTurn(targetBoard); // Rekurencyjne wywołanie tury dla tego samego gracza
+                return;
+            }
+
+            // Obsługa trafienia lub spudłowania
+            if (targetBoard[row, col] == 'X')
+            {
+                Console.WriteLine("You hit a ship!"); // Gracz trafił statek przeciwnika
+                targetBoard[row, col] = '!'; // Oznaczenie trafienia na planszy przeciwnika
+                if (targetBoard == boardPlayer1)
+                    shipsRemainingPlayer1--; // Zmniejszenie liczby pozostałych statków gracza 1
+                else
+                    shipsRemainingPlayer2--; // Zmniejszenie liczby pozostałych statków gracza 2
+            }
+            else if (targetBoard[row, col] == '!' || targetBoard[row, col] == '-')
+            {
+                Console.WriteLine("You missed."); // Gracz spudłował
+                targetBoard[row, col] = '#'; // Oznaczenie chybionego strzału na planszy przeciwnika
+            }
+        }
+
+        // Wyświetlenie planszy
+        static void DisplayBoard(char[,] board)
+        {
+            Console.WriteLine("   0 1 2 3 4 5 6 7 8 9");
             for (int row = 0; row < 10; row++)
             {
                 Console.Write((char)('A' + row) + "  "); // Wyświetlanie indeksów wierszy
@@ -114,7 +150,7 @@ namespace BattleshipsGame
                         Console.Write("- "); // Wyświetlanie nieodkrytych pól statków w trybie debugowania
                     }
                 }
-                Console.WriteLine();
+                Console.WriteLine(); // Nowa linia po każdym wierszu
             }
         }
     }
